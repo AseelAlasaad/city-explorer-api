a3d530130f0774ba673198b4b3ef7c368ee93898
Aseel Alasaad <aseelalasaad@gmail.com>
2021-09-07 19:33:02 +0300

    finish

--- server.js
+++ server.js
@@ -3,14 +3,18 @@
 require('dotenv').config();
 const express=require('express');
 const cors=require('cors');
+
+//axios
+const axios=require('axios');
+
 //get wather data
-const weatherDate=require('./data/weather.json');
+// const weatherDate=require('./data/weather.json');
 
 const server=express();
 server.use(cors());
 //get port from .env file
 const PORT=process.env.PORT;
-//class Forecast
+
 
 
 
@@ -19,28 +23,53 @@ const PORT=process.env.PORT;
 server.get('/',(req,res)=>{
     res.send('Welcome from the home');
 })
-
-// http://localhost:3010/getWeatherinfo?cityName=Amman
+//http://api.weatherbit.io/v2.0/history/daily/getWeatherinfo
+// http://localhost:3010/getWeatherinfo?lat=''&lon=''
 server.get('/getWeatherinfo',(req,res)=>{
- const cityName=req.query.cityName;
+   const cityName=req.query.cityName;
     const lat=req.query.lat;
     const lon=req.query.lon;
-    const returnArr=weatherDate.find((item)=>{
-      if(item.city_name===cityName)
-      {   
-          return item;
-      }
+    // &lat=${lat}&lon=${lon}
+    let url=`http://api.weatherbit.io/v2.0/forecast/daily/getWeatherinfo?lat=${lat}&lon=${lon}&Key=${process.env.WEATHER_API_KEY}`;
+    let weatherInfo=[];
+    axios.get(url).then(result=>{
+    //   console.log(result);
+      weatherInfo=result.data.data.map(item=>{
+         return new Forecast(item);
+     })
+
+     res.send(weatherInfo);
     })
-    const weatherArr=returnArr.data.map((item)=>{
-       let obj =new Forecast(item);
-       return obj ;
-    }) 
+    .catch((error)=>{
+        res.status(500).send('Error!');
+    })
 
- console.log(weatherArr);
 
- res.send(weatherArr);
+
+
 });
-
+//http://localhost:3010/getMovie?cityName=Amman
+server.get('/getMovie',(req,res)=>{
+    const cityName=req.query.cityName;
+     let urlmovie=`https://api.themoviedb.org/3/search/getMovie?api_key=${process.env.MOVIE_API_KEY}&cityName=${cityName}`;
+     let movieInfo=[];
+     axios.get(urlmovie).then(result=>{
+     //   console.log(result);
+       movieInfo=result.data.results.map(item=>{
+          return new Movie(item);
+      })
+ 
+      res.send(movieInfo);
+     })
+     .catch((error)=>{
+         res.status(500).send('Error!');
+     })
+ 
+ 
+ 
+ 
+ });
+ 
 
 class Forecast{
 
@@ -50,6 +79,19 @@ class Forecast{
         this.date=item.valid_date;
         
         
+    }
+}
+
+class Movie{
+
+    constructor(item){
+      this.title=item.title;
+      this.overview=item.overview;
+      this.vote_average=item.vote_average;
+      this.vote_count=item.vote_count;
+      this.popularity=item.popularity;
+      this.release=item.release;  
+    
     }
 }
 server.get('*',(req,res)=>{
